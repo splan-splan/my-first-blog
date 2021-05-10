df86fb1897ace91fbc3b75e05fcf790b6c67bd3e
splan-splan <welcome@splan.tokyo>
2021-05-10 17:09:54 +0900

    Add view and template for detailed blog post as well as CSS for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# import hello.views as hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+   
 ]
